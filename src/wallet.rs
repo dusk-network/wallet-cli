0abc508e08ff32ac71b193b6019321668eb6fc8e
Daksh <work@dakshu.xyz>
2023-06-06 23:02:59 +0530

    Create StateStore which impls State and Store; Use nullifier as key of cache value

--- src/wallet.rs
+++ src/wallet.rs
@@ -31,7 +31,7 @@ use dusk_wallet_core::{
 use rand::prelude::StdRng;
 use rand::SeedableRng;
 
-use crate::clients::{Prover, State};
+use crate::clients::{Prover, StateStore};
 use crate::crypto::{decrypt, encrypt};
 use crate::currency::Dusk;
 use crate::rusk::{RuskClient, RuskEndpoint};
@@ -60,7 +60,7 @@ const VERSION: &[u8] = &[2, 0];
 /// able to perform common operations such as checking balance, transfernig
 /// funds, or staking Dusk.
 pub struct Wallet<F: SecureWalletFile + Debug> {
-    wallet: Option<WalletCore<LocalStore, State, Prover>>,
+    wallet: Option<WalletCore<LocalStore, StateStore, Prover>>,
     addresses: Vec<Address>,
     store: LocalStore,
     file: Option<F>,
@@ -296,7 +296,9 @@ impl<F: SecureWalletFile + Debug> Wallet<F> {
         };
 
         // create a state client
-        let mut state = State::new(rusk.state, &cache_dir)?;
+        let mut state =
+            StateStore::new(rusk.state, &cache_dir, self.store.clone())?;
+
         state.set_status_callback(status);
 
         // create wallet instance
