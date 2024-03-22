211a33883c5742be78f87d7998b84c9c611823b3
Herr Seppia <seppia@dusk.network>
2024-03-22 19:14:00 +0100

    Change rusk-wallet to wait for tx to be included

--- src/bin/io/gql.rs
+++ src/bin/io/gql.rs
@@ -71,9 +71,7 @@ impl GraphQL {
 
     /// Wait for a transaction to be confirmed (included in a block)
     pub async fn wait_for(&self, tx_id: &str) -> anyhow::Result<()> {
-        const TIMEOUT_SECS: i32 = 30;
-        let mut i = 1;
-        while i <= TIMEOUT_SECS {
+        loop {
             let status = self.tx_status(tx_id).await?;
 
             match status {
@@ -81,14 +79,9 @@ impl GraphQL {
                 TxStatus::Error(err) => return Err(Error::Transaction(err))?,
                 TxStatus::NotFound => {
                     (self.status)(
-                        format!(
-                            "Waiting for confirmation... ({}/{})",
-                            i, TIMEOUT_SECS
-                        )
-                        .as_str(),
+                        "Waiting for tx to be included into a block...",
                     );
                     sleep(Duration::from_millis(1000)).await;
-                    i += 1;
                 }
             }
         }
